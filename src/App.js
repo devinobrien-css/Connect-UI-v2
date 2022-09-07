7243bd453eefd0ebf95c0acf999856ca701797ac
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2022-09-07 10:23:48 -0400

    Test stackblitz connection to tailwind

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "./index.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see cool neat magic happen :)</p>
+      <p className="text-gray-500 border-solid border-2 border-gray-300 rounded">test</p>
     </div>
   );
 }
