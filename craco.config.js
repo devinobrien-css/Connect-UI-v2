9969e3f9cd9330162da83ce4de12c2fe8b97af0a
Devin O'Brien <61803565+devinobrien-css@users.noreply.github.com>
2022-09-07 10:17:46 -0400

    Initial Commit
    tailwindcss installed and set up

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+//from tailwindCSS docs
+module.exports = {
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./style.css";
+import "./index.css";
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Start editing to see cool neat magic happen :)</p>
     </div>
   );
 }
