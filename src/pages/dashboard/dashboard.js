ff2feaf26671ff05a33c38a323b68f404692fc43
devinobrien-css <61803565+devinobrien-css@users.noreply.github.com>
2022-09-09 10:03:25 -0400

    stackblitz attempt

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,37 +1,36 @@
-import { useRecoilState } from "recoil"
-import { userState } from '../../atoms.js'
-import DashboardPortal from './dashboard_portal'
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { userState } from '../../atoms.js';
+import DashboardPortal from './dashboard_portal';
 
 /** Header of Dashboard
- * 
- * @returns 
+ *
+ * @returns
  */
 const DashboardHeader = () => {
-    const [user,setUser] = useRecoilState(userState)
+  const [user, setUser] = useRecoilState(userState);
 
-    console.log(user)
-    return (
-        <div className='bg-gray-900 rounded-xl w-full py-3 px-2'>
-            <p className='text-gray-100 text-2xl'>Hello there, {user.first}</p>
-        </div>
-    )
-}
+  console.log(user);
+  return (
+    <div className="bg-gray-900 rounded-2xl w-full py-3 px-2">
+      <p className="text-gray-100 text-2xl rounded-2xl dark:text-gray-200">
+        Hello there, {user.first}
+      </p>
+    </div>
+  );
+};
 
 /** Launches the dashboard
- * 
+ *
  * @returns the content of the dashboard page
  */
 const Dashboard = () => {
-
-    return (
-        <>
-            <DashboardHeader />
-            <DashboardPortal />
-        </>
-    )
-}
-
-
-
+  return (
+    <>
+      <DashboardHeader />
+      <DashboardPortal />
+    </>
+  );
+};
 
 export default Dashboard;
